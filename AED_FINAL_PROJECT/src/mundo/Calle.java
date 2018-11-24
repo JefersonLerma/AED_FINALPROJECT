8a42099457ea136f4c48357dbf7e8eaa98464e22
jeffe <jeffe@DESKTOP-SOUHA69>
2018-11-24 18:39:41 -0500

    Agrego la clase calle

--- AED_FINAL_PROJECT/src/mundo/Calle.java
+++ AED_FINAL_PROJECT/src/mundo/Calle.java
@@ -1,5 +1,40 @@
 package mundo;
 
 public class Calle {
+	
+	private String nombreCalle;
+	
+	private String[] paradas;
+	
+	public Calle(String pNombreCalles, String[] pParadas) {
+		nombreCalle = pNombreCalles;
+		paradas = pParadas;
+	}
+	
+	public String darNombreCalle() {
+		return nombreCalle;
+	}
+	
+	public void cambiarNombreCalle(String pNombreCalle) {
+		nombreCalle = pNombreCalle;
+	}
+	
+	public String[] darParadas() {
+		return paradas;
+	}
+	
+	public void cambiarParadas(String[] pParadas) {
+		paradas = pParadas;
+	}
+	
+	public String toString() {
+		String mensaje = "";
+		
+		mensaje += nombreCalle +", Paradas: - ";
+		for(int i = 0; i<paradas.length; i++) {
+			mensaje += paradas[i] +" - ";
+		}
+		return mensaje;
+	}
 
 }
