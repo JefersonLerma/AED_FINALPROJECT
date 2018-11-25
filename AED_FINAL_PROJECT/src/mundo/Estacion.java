045e6fb732d45c34ad6ccd96a384b7c93190460c
jeffe <jeffe@DESKTOP-SOUHA69>
2018-11-24 20:10:17 -0500

    Adiciono algunos metodos a esta clase

--- AED_FINAL_PROJECT/src/mundo/Estacion.java
+++ AED_FINAL_PROJECT/src/mundo/Estacion.java
@@ -1,5 +1,14 @@
 package mundo;
 
+import grafo.lista.GrafoLista;
+import grafo.matriz.GrafoMatriz;
+
 public class Estacion {
 
+	private String nombreEStacion;
+    private Calle[] lasCalles;
+	
+	
+	
+	
 }
