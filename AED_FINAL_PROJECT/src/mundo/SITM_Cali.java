c9b89746ff79b34adcdfd2f66c2049689471b26f
jeffe <jeffe@DESKTOP-SOUHA69>
2018-11-24 20:40:59 -0500

    Cree y agregue los primero metodos de la clase principal del mundo

---
+++ AED_FINAL_PROJECT/src/mundo/SITM_Cali.java
@@ -0,0 +1,44 @@
+package mundo;
+
+import grafo.lista.GrafoLista;
+import grafo.matriz.GrafoMatriz;
+
+public class SITM_Cali {
+	
+	private String nombre;
+	
+	private GrafoLista<Estacion> listaEstacion;
+	
+	private GrafoMatriz<Estacion> matrizEstacion;
+	
+	public SITM_Cali() {
+		nombre = "MIO";
+		listaEstacion = new GrafoLista<Estacion>();
+		matrizEstacion = new GrafoMatriz<Estacion>();
+	}
+	
+	public String darNombre() {
+		return nombre;
+	}
+	
+	public void cambiarNombre(String pNombre) {
+		nombre = pNombre;
+	}
+	
+	public GrafoLista<Estacion> darListaEstacion(){
+		return listaEstacion;
+	}
+	
+	public void cambiarListaEstacion(GrafoLista<Estacion> pListaEstacion) {
+		listaEstacion = pListaEstacion;
+	}
+	
+	public GrafoMatriz<Estacion> darMatrizEstacion(){
+		return matrizEstacion;
+	}
+	
+	public void cambiarMatrizEstacion(GrafoMatriz<Estacion> pMatrizEstacion) {
+		matrizEstacion = pMatrizEstacion;
+	}
+
+}
